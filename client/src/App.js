e5519848fc4434631c3a7c2fc3fa6902167f4eb9
Ahmed Ibrahim <ib153507@gmail.com>
2022-06-12 17:35:20 +0200

    Remove App.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
-import Dashboard from './components/Dashboard';
-import store from './store';
-import { Provider } from 'react-redux';
-import Navbar from './components/layouts/Navbar';
+import "./index.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import store from "./store";
+import { Provider } from "react-redux";
+import Navbar from "./components/layouts/Navbar";
 function App() {
   return (
     <Provider store={store}>
       <Router>
-        <div className='h-screen overflow-x-hidden'>
+        <div className="h-screen overflow-x-hidden">
           <Switch>
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
-            <Route  path='/' component={Dashboard} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route path="/" component={Dashboard} />
           </Switch>
         </div>
       </Router>
